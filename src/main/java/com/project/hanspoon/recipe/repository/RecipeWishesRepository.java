796040e9d1ac4c26970c12e603d89ce2a03fb1b4
“LeeHanNa” <gkssk122827@gmail.com>
2026-02-26 12:59:12 +0900

    레시피 (한나) 0226 관심목록 AM

--- src/main/java/com/project/hanspoon/recipe/repository/RecipeWishesRepository.java
+++ src/main/java/com/project/hanspoon/recipe/repository/RecipeWishesRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface RecipeWishesRepository extends JpaRepository<RecipeWish, Long> {
     List<RecipeWish> recipe(Recipe recipe);
@@ -26,4 +26,6 @@ public interface RecipeWishesRepository extends JpaRepository<RecipeWish, Long> 
     void deleteByUserEmailAndId(String email, Long id);
 
     boolean existsByUserEmailAndRecipeId(String email, Long id);
+
+    Optional<RecipeWish> findByUserEmailAndRecipeId(String userEmail, Long id);
 }
